3fb81bfbf537a849f40b88dcec126879cf10609c
Vladislav Tupikin <mrrefactoring@yandex.ru>
2021-12-09 14:52:00 +0300

    tests fixes

--- src/clients/client.ts
+++ src/clients/client.ts
@@ -2,10 +2,10 @@ import type { Callback } from '../callback';
 import type { RequestConfig } from '../requestConfig';
 
 export interface Client {
-  sendRequest<T>(requestConfig: RequestConfig, callback?: never, telemetryData?: Partial<any>): Promise<T>;
+  sendRequest<T>(requestConfig: RequestConfig, callback?: never, telemetryData?: any): Promise<T>;
   sendRequest<T>(
     requestConfig: RequestConfig,
     callback?: Callback<T>,
-    telemetryData?: Partial<any>
+    telemetryData?: any,
   ): Promise<void>;
 }
