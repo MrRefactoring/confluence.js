6002085ae9455971fc10bba5a43206eb87f4d928
Andrew McClenaghan <amcclenaghan@atlassian.com>
2023-03-02 11:56:10 +1000

    Add collectionName to help with attaching images to confluence pages in ADF

--- src/api/models/attachment.ts
+++ src/api/models/attachment.ts
@@ -14,6 +14,7 @@ export interface Attachment {
     comment: string;
     mediaTypeDescription: string;
     fileId: string;
+    collectionName: string;
   };
   _expandable: {
     childTypes: string;
