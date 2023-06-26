6d7f395d30de6d34c7a765bff6493b2322b858a7
Mukul <mukultotla4@gmail.com>
2023-06-26 23:11:13 +0530

    fix(getInlineCommentVersions): dashed to camelCase convention

--- src/version2/parameters/getInlineCommentVersions.ts
+++ src/version2/parameters/getInlineCommentVersions.ts
@@ -5,7 +5,7 @@ export interface GetInlineCommentVersions {
    * The content format types to be returned in the `body` field of the response. If available, the representation will
    * be available under a response field of the same name under the `body` field.
    */
-  'body-format'?: {};
+  'bodyFormat'?: {};
   /**
    * Used for pagination, this opaque cursor will be returned in the `next` URL in the `Link` response header. Use the
    * relative URL in the `Link` header to retrieve the `next` set of results.
