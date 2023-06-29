d89f1f0b8dfb2d4945ac3cad34ecbc321fa237f4
Fabio Rodriguez <71491674+frodri13@users.noreply.github.com>
2023-06-29 17:23:39 +0000

    modified interface

--- src/version2/parameters/getBlogPostVersions.ts
+++ src/version2/parameters/getBlogPostVersions.ts
@@ -8,7 +8,7 @@ export interface GetBlogPostVersions {
    * The content format types to be returned in the `body` field of the response. If available, the representation will
    * be available under a response field of the same name under the `body` field.
    */
-  'body-format'?: {};
+  bodyFormat?: {};
   /**
    * Used for pagination, this opaque cursor will be returned in the `next` URL in the `Link` response header. Use the
    * relative URL in the `Link` header to retrieve the `next` set of results.
