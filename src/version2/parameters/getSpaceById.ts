845ba31f65820b427347d4fbe67ebb76dfebe42d
Fabio Rodriguez <71491674+frodri13@users.noreply.github.com>
2023-06-29 17:29:46 +0000

    modified interface

--- src/version2/parameters/getSpaceById.ts
+++ src/version2/parameters/getSpaceById.ts
@@ -5,5 +5,5 @@ export interface GetSpaceById {
    * The content format type to be returned in the `description` field of the response. If available, the representation
    * will be available under a response field of the same name under the `description` field.
    */
-  'description-format'?: {};
+  descriptionFormat?: {};
 }
