6bb64a06427651397cd9a2de66fc0367e557bd97
Fabio Rodriguez <71491674+frodri13@users.noreply.github.com>
2023-06-30 03:29:33 +0000

    modified interface

--- src/version2/parameters/getSpaces.ts
+++ src/version2/parameters/getSpaces.ts
@@ -15,7 +15,7 @@ export interface GetSpaces {
    * The content format type to be returned in the `description` field of the response. If available, the representation
    * will be available under a response field of the same name under the `description` field.
    */
-  'description-format'?: {};
+  descriptionFormat?: {};
   /**
    * Used for pagination, this opaque cursor will be returned in the `next` URL in the `Link` response header. Use the
    * relative URL in the `Link` header to retrieve the `next` set of results.
