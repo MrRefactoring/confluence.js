9808944f76ca9240e1a0a7cd33e1e2e0027efd15
Fabio Rodriguez <71491674+frodri13@users.noreply.github.com>
2023-06-30 05:11:58 +0000

    modified GetLabelPages interface

--- src/version2/parameters/getLabelPages.ts
+++ src/version2/parameters/getLabelPages.ts
@@ -5,7 +5,7 @@ export interface GetLabelPages {
    * The content format types to be returned in the `body` field of the response. If available, the representation will
    * be available under a response field of the same name under the `body` field.
    */
-  'body-format'?: {};
+  bodyFormat?: {};
   /** Used to sort the result by a particular field. */
   sort?: 'id' | '-id' | 'created-date' | '-created-date' | 'modified-date' | '-modified-date';
   /**
