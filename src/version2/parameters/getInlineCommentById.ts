b9fa1caf982a69da34d1a9b907b38eb49b52a21b
Fabio Rodriguez <71491674+frodri13@users.noreply.github.com>
2023-07-01 20:58:50 +0000

    modified interface

--- src/version2/parameters/getInlineCommentById.ts
+++ src/version2/parameters/getInlineCommentById.ts
@@ -5,7 +5,7 @@ export interface GetInlineCommentById {
    * The content format type to be returned in the `body` field of the response. If available, the representation will
    * be available under a response field of the same name under the `body` field.
    */
-  'body-format'?: {};
+  bodyFormat?: {};
   /**
    * Allows you to retrieve a previously published version. Specify the previous version's number to retrieve its
    * details.
