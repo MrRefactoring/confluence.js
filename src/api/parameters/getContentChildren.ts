bbb00d8ffe1643084764ce7563141e28b05e760c
Vladislav Tupikin <MrRefactoring@yandex.ru>
2025-07-17 00:12:43 +0300

    #145: add `start` and `limit` properties for `contentChildrenAndDescendants.getContentChildren` (#148)

--- src/api/parameters/getContentChildren.ts
+++ src/api/parameters/getContentChildren.ts
@@ -1,3 +1,5 @@
+import type { OneOrMany } from '../../interfaces';
+
 export interface GetContentChildren {
   /** The ID of the content to be queried for its children. */
   id: string;
@@ -9,7 +11,11 @@ export interface GetContentChildren {
    * - `page` returns all child pages of the content.
    * - Custom content types that are provided by apps are also supported.
    */
-  expand?: string[];
+  expand?: OneOrMany<'attachment' | 'comments' | 'page' | string>;
   /** The version of the parent content to retrieve children for. Currently, this only works for the latest version. */
   parentVersion?: number;
+  /** The starting index of the returned content children. */
+  start?: number;
+  /** The maximum number of content children to return per page. */
+  limit?: number;
 }
